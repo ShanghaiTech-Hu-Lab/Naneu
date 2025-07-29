ad831da3acbd0309faffa4f03c641cd23998851c
5o1 <assanekowww@gmail.com>
2025-07-29 21:44:31 +0800

    update TorchExtraContext

--- helpers/context.py
+++ helpers/context.py
@@ -1,7 +1,7 @@
 import warnings
 import torch
 from torch import nn
-from typing import Callable
+from typing import Callable, Literal, List
 from collections import OrderedDict
 
 class ExtraContext:
@@ -18,35 +18,45 @@ class ExtraContext:
             model.extra_context.add_hook("hook_name", hook_function)
             model.extra_context.add_object("object_name", some_object)
     """
+    ReduceOps = Literal["sum", "mean", "max", "min"]
 
 
     def __init__(self, context: nn.Module, logger:Callable = None):
         self.__context = context
         self.logger = logger
 
-        self.__registed_module = OrderedDict()
-        self.__extra_losses = []
-        self.__extra_hooks = []
+        self.__registed_modules = OrderedDict()
+        self.__registed_modules_prefix = OrderedDict()
+        self.__extra_losses = dict() # lossname: List[lossitem]
+        self.__extra_metrics = dict() # metricsname: List[metricsitem]
+        self.__extra_hooks = dict()
+        self.__extra_objects = dict()
 
-        self.__extra_objects = {}
+        self.__op_losses = dict()
+        self.__op_metrics = dict()
 
     def __enter__(self):
         for prefix, module in self.__context.named_modules():
-            if module in self.__registed_module:
+            module_id = id(module)
+            if module_id in self.__registed_modules:
                 warnings.warn(f"Submodule{prefix} is contained multiple times by different modules. This can lead to potential problems.", UserWarning)
-                self.__registed_module[module].append(prefix)
+                self.__registed_modules[module_id] = module
+                self.__registed_modules_prefix[module_id].append(prefix)
             else:
-                self.__registed_module[module] = [prefix,]
                 if hasattr(module, "extra_context"):
-                    raise ValueError(f"Name domain conflict. The module {prefix} already has `extra_context`.")
+                    raise ValueError(f"Name domain conflict. The module {module._get_name()} in {prefix}, already has `extra_context`.")
                 setattr(module, "extra_context", self)
+                self.__registed_modules[module_id] = module
+                self.__registed_modules_prefix[module_id] = [prefix,]
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.__extra_losses = None
+        self.__extra_metrics = None
         self.__extra_hooks = None
         self.__extra_objects = None
-        for module in self.__registed_module.keys():
+
+        for module_id, module in self.__registed_modules.items():
             if not hasattr(module, "extra_context"):
                 raise RuntimeError(
                     f"Module {module.__class__.__name__} has no context to remove."
@@ -63,21 +73,31 @@ class ExtraContext:
             raise ValueError("Objects dict has been cleared. Users should not access context manager after exiting context. This could be a bug.")
         self.__extra_objects[key] = value
 
-    def add_loss(self, prefix:str, loss: torch.Tensor):
+    def add_loss(self, prefix: str, loss: torch.Tensor, op: ReduceOps = "mean"):
         """
         Add a loss term to the context.
         """
         if self.__extra_losses is None:
             raise ValueError("Losses have been cleared. Users should not access context manager after exiting context. This could be a bug.")
-        self.__extra_losses.append((prefix, loss))
+        self.__extra_losses.setdefault(prefix, []).append(loss)
+        self.__op_losses[prefix] = op
 
-    def add_hook(self, prefix:str, hook: Callable):
+    def add_metric(self, prefix: str, metric: torch.Tensor, op: ReduceOps = "mean"):
+        """
+        Add a metric term to the context.
+        """
+        if self.__extra_metrics is None:
+            raise ValueError("Metrics have been cleared. Users should not access context manager after exiting context. This could be a bug.")
+        self.__extra_metrics.setdefault(prefix, []).append(metric)
+        self.__op_metrics[prefix] = op
+
+    def add_hook(self, prefix: str, hook: Callable):
         """
         Add a hook to the context.
         """
         if self.__extra_hooks is None:
             raise ValueError("Hooks have been cleared. Users should not access context manager after exiting context. This could be a bug.")
-        self.__extra_hooks.append((prefix, hook))
+        self.__extra_hooks.setdefault(prefix, []).append(hook)
 
     def log(self, *args, **kwargs):
         if self.logger is None:
@@ -85,6 +105,50 @@ class ExtraContext:
             return
         res = self.logger(*args, **kwargs)
         return res
+    
+
+    def _tensors_reduce(self, tensors: List[torch.Tensor], op: ReduceOps = "mean"):
+        """
+        Reduce a tensor using the specified operation.
+        """
+        if len(tensors) == 0:
+            return None
+        if len(tensors) == 1:
+            return tensors[0]
+        if op == "sum":
+            return torch.sum(torch.stack(tensors, dim=0), dim=0)
+        elif op == "mean":
+            return torch.mean(torch.stack(tensors, dim=0), dim=0)
+        elif op == "max":
+            return torch.max(torch.stack(tensors, dim=0), dim=0).values
+        elif op == "min":
+            return torch.min(torch.stack(tensors, dim=0), dim=0).values
+        else:
+            raise ValueError(f"Unsupported operation {op}. Supported operations are {ExtraContext.ReduceOps}.")
+
+    def get_losses(self, default_op: ReduceOps = "mean"):
+        """
+        Get the registered losses in the context.
+        """
+        if self.__extra_losses is None:
+            raise ValueError("Losses have been cleared. Users should not access context manager after exiting context. This could be a bug.")
+        
+        losses = {}
+        for loss_name, loss_terms in self.__extra_losses.items():
+            losses[loss_name] = self._tensors_reduce(loss_terms, op=self.__op_losses.get(loss_name, default_op))
+        return losses
+
+    def get_metrics(self, default_op: ReduceOps = "mean"):
+        """
+        Get the registered metrics in the context.
+        """
+        if self.__extra_metrics is None:
+            raise ValueError("Metrics have been cleared. Users should not access context manager after exiting context. This could be a bug.")
+        
+        metrics = {}
+        for metric_name, metric_terms in self.__extra_metrics.items():
+            metrics[metric_name] = self._tensors_reduce(metric_terms, op=self.__op_metrics.get(metric_name, default_op))
+        return metrics
     
     @property
     def losses(self):
@@ -97,9 +161,9 @@ class ExtraContext:
         if self.__extra_hooks is None:
             raise ValueError("Hooks have been cleared. Users should not access context manager after exiting context. This could be a bug.")
         return self.__extra_hooks
-        
 
-def register_extra_loss(module: nn.Module, loss_term: torch.Tensor, prefix: str = None):
+
+def register_extra_loss(module: nn.Module, loss_term: torch.Tensor, prefix: str = None, op: ExtraContext.ReduceOps = "mean"):
     """
     Register an extra loss term to a module.
     """
@@ -107,7 +171,19 @@ def register_extra_loss(module: nn.Module, loss_term: torch.Tensor, prefix: str 
         if module.training:
             warnings.warn(f"Training does not launch with an ExtraContext. This loss will be ignored.", UserWarning,stacklevel=2)
         return
-    module.extra_context.add_loss(prefix, loss_term)
+    module.extra_context.add_loss(prefix, loss_term, op=op)
+
+
+def register_extra_metric(module: nn.Module, metric_term: torch.Tensor, prefix: str = None, op: ExtraContext.ReduceOps = "mean"):
+    """
+    Register an extra metric term to a module.
+    """
+    if not hasattr(module, "extra_context"):
+        if module.training:
+            warnings.warn(f"Training does not launch with an ExtraContext. This metric will be ignored.", UserWarning, stacklevel=2)
+        return
+    module.extra_context.add_metric(prefix, metric_term, op=op)
+
 
 def register_extra_hook(module: nn.Module, hook: Callable, prefix: str = None):
     """
@@ -119,6 +195,7 @@ def register_extra_hook(module: nn.Module, hook: Callable, prefix: str = None):
         return
     module.extra_context.add_hook(prefix, hook)
 
+
 def get_extra_context(module: nn.Module):
     """
     Get the extra context of a module.
@@ -128,6 +205,7 @@ def get_extra_context(module: nn.Module):
             warnings.warn(f"Training does not launch with an ExtraContext. This will return None.", UserWarning, stacklevel=2)
         return None
     return module.extra_context
+
 
 def log_extra(module: nn.Module, *args, **kwargs):
     """
